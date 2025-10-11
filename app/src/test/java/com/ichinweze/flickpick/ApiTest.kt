1797a052a2591dd89cafa6ddb291827f6277d42d
aichinweze <aichinweze@gmail.com>
2025-10-11 13:48:31 -0400

    Updating test scenarios for API call. Updating imports for screen strings.

--- app/src/test/java/com/ichinweze/flickpick/ApiTest.kt
+++ app/src/test/java/com/ichinweze/flickpick/ApiTest.kt
@@ -1,5 +1,6 @@
 package com.ichinweze.flickpick
 
+import com.ichinweze.flickpick.data.ApiData.MovieGenreResults
 import com.ichinweze.flickpick.data.ApiData.MoviePageResult
 import com.ichinweze.flickpick.interfaces.MovieApiService
 import org.junit.Test
@@ -21,9 +22,6 @@ class ApiTest {
     val testUrl = "discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=popularity.desc&with_genres=28&with_origin_country=US&release_date.gte=2000-01-01&vote_average.gte=6&vote_average.lte=7&with_runtime.gte=100"
     val authToken: String = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDBmZGZlNGY2OWY3MWY5YzFkNzJiNTAwZDBmMDE3OSIsIm5iZiI6MTc1OTU5OTE0OC4yNjIsInN1YiI6IjY4ZTE1YTJjYTIyNjYyN2FjZjI1YWIzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ijkX6Eg1pTo-lZUChSjigXDTbl7ei33bUNtzqu6mFOA"
 
-    val thing1 = "discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=vote_average.desc&page=1&with_origin_country=GB&release_date.gte=2010-01-01&release_date.lte=2020-12-31&vote_average.gte=7&vote_average.lte=8.5&with_runtime.gte=2010&with_runtime.lte=2020"
-    val thing2 = "discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=vote_average.desc&page=2&with_origin_country=GB&release_date.gte=2010-01-01&release_date.lte=2020-12-31&vote_average.gte=7&vote_average.lte=8.5&with_runtime.gte=2010&with_runtime.lte=2020"
-
     @Test
     fun getMovieResponseFromApi() {
         var page = 0
@@ -42,22 +40,52 @@ class ApiTest {
         println("getMovieResponseFromApi: status = $status, success = ${movieApiResponse.isSuccessful}")
 
         if (responseBody != null) {
-            page = responseBody!!.page
-            totalPages = responseBody!!.totalPages
-            totalResults = responseBody!!.totalResults
+            page = responseBody.page
+            totalPages = responseBody.totalPages
+            totalResults = responseBody.totalResults
 
-            results = responseBody!!.results
+            results = responseBody.results
 
             println("getMovieResponseFromApi: page = $page")
             println("getMovieResponseFromApi: total pages = $totalPages")
             println("getMovieResponseFromApi: total results = $totalResults")
             println("getMovieResponseFromApi: results: $results")
-        } else {
-            println("getMovieResponseFromApi: Empty response")
         }
 
         assert(results.isNotEmpty()) { "No results returned" }
         assert(page == 1) { "Unexpected page returned: $page" }
         assert(movieApiResponse.isSuccessful) { "API Request unsuccessful" }
+        assert(responseBody != null) { "Empty response" }
+    }
+
+    @Test
+    fun getGenreIdsFromApi() {
+        var genres = listOf<MovieGenreResults>()
+
+        val unexpectedGenreIds = genres.filter { genre -> genre.id < 0 }
+        val unexpectedGenreValues = genres.filter { genre -> genre.name == "" }
+
+        val movieApiResponse = movieApiService
+            .getMovieGenreIds(authToken = authToken)
+            .execute()
+
+        val responseBody = movieApiResponse.body()
+        val status = movieApiResponse.code()
+
+        println("getGenreIdsFromApi: status = $status, success = ${movieApiResponse.isSuccessful}")
+
+        if (responseBody != null) {
+            genres = responseBody.genres
+
+            println("getGenreIdsFromApi: number of genres = ${genres.size}")
+        }
+
+        assert(genres.isNotEmpty()) { "No results returned" }
+        assert(unexpectedGenreIds.isEmpty()) { "Unexpected genre ID returned: $unexpectedGenreIds" }
+        assert(unexpectedGenreValues.isEmpty()) {
+            "Unexpected genre values: count = ${unexpectedGenreValues.size}"
+        }
+        assert(movieApiResponse.isSuccessful) { "API Request unsuccessful" }
+        assert(responseBody != null) { "Empty response" }
     }
 }
